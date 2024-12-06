a011f96632073d941a8000318d502173e5d73efb
Michael Maroszek <paresy@gmail.com>
2024-12-06 09:46:36 +0100

    fix error when reseting file

--- PCT14 Configurator/module.php
+++ PCT14 Configurator/module.php
@@ -40,7 +40,7 @@ declare(strict_types=1);
         private function createConfiguratorValues(String $File)
         {
             if (strlen($File) == 0) {
-                return false;
+                return [];
             }
             $xml = simplexml_load_string(base64_decode($File), null, LIBXML_NOCDATA);
 
